fbe3aaf48fcb660e82a9103cf46fff4e63bf30e3
Caikekeweb <caikk@addcn.com>
2022-03-07 10:31:41 +0800

    feat(app): 新增自定義打包

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 set -euo pipefail
 
-EM_VERSION=2.0.24
+EM_VERSION=2.0.8
 
 docker pull emscripten/emsdk:$EM_VERSION
+# -v $PWD/wasm/cache:/emsdk_portable/.data/cache/wasm \
 docker run \
   --rm \
   -v $PWD:/src \
-  -v $PWD/wasm/cache:/emsdk_portable/.data/cache/wasm \
-  -e FFMPEG_ST=${FFMPEG_ST:-no} \
   emscripten/emsdk:$EM_VERSION \
-  bash ./build.sh "$@"
+  sh -c 'bash ./build1.sh'
