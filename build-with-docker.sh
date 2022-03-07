888f0aec6347a709a67e8dab9e14a7269f1657cd
Caikekeweb <caikk@addcn.com>
2022-03-07 11:25:41 +0800

    feat(app): 還原打包配置

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 set -euo pipefail
 
-EM_VERSION=2.0.8
+EM_VERSION=2.0.24
 
 docker pull emscripten/emsdk:$EM_VERSION
-# -v $PWD/wasm/cache:/emsdk_portable/.data/cache/wasm \
 docker run \
   --rm \
   -v $PWD:/src \
+  -v $PWD/wasm/cache:/emsdk_portable/.data/cache/wasm \
+  -e FFMPEG_ST=${FFMPEG_ST:-no} \
   emscripten/emsdk:$EM_VERSION \
-  sh -c 'bash ./build1.sh'
+  bash ./build.sh "$@"
